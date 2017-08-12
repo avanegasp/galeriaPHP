2d9598971c400cf584df446fa4fe21bfbdd295f3
avanegasp <arco527@gmail.com>
2017-08-12 17:15:50 -0500

    Add css

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <header>
-      <div class="contenedor">
-        <h1 class="titulo">Mi íncreible galería para subastar</h1>
-      </div>
-    </header>
+<?php 
 
-    <section class="fotos">
-      <div class="contenedor">
-        <div class="thumb">
-          <a href="#">
-            <img src="imagenes/1.jpg" alt="">
-          </a>
-        </div>
-      </div>
-    </section>
-  </body>
-</html>
+require 'funciones.php';
+$fotos_por_pagina = 8;
+
+$pagina_actual = (isset($_GET['p']) ? (int)$_GET['p'] : 1);
+$inicio = ($pagina_actual > 1) ? $pagina_actual * $fotos_por_pagina - $fotos_por_pagina : 0;
+
+$conexion = conexion('galeria_practica', 'root', '');
+
+if (!$conexion) {
+	die();
+}
+
+$statement = $conexion->prepare("
+	SELECT SQL_CALC_FOUND_ROWS * FROM fotos LIMIT $inicio, $fotos_por_pagina
+");
+
+$statement->execute();
+$fotos = $statement->fetchAll();
+
+if (!$fotos) {
+	header('Location: index.php');
+}
+
+$statement = $conexion->prepare("SELECT FOUND_ROWS() as total_filas");
+$statement->execute();
+$total_post = $statement->fetch()['total_filas'];
+
+$total_paginas = ceil($total_post / $fotos_por_pagina);
+
+
+require 'views/index.view.php';
+
+?>
