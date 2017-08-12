4c382f319f700b67cbdd751f2100cff1469dd530
avanegasp <arco527@gmail.com>
2017-08-12 17:11:56 -0500

    Add index files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <div class="contenedor">
+        <h1 class="titulo">Mi íncreible galería para subastar</h1>
+      </div>
+    </header>
+
+    <section class="fotos">
+      <div class="contenedor">
+        <div class="thumb">
+          <a href="#">
+            <img src="imagenes/1.jpg" alt="">
+          </a>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
